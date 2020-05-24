f2369ca12f989e0c44fd1f85d6f4639edaf7c5c1
Curt Cox <curt@Curts-MacBook-Pro.local>
2020-05-24 18:50:33 -0500

    Extract methods. - Curt

--- AnkiVehicleController/StdInReader.m
+++ AnkiVehicleController/StdInReader.m
@@ -42,47 +42,71 @@
                      options:0
                      error:&error];
         if (error) {
-            NSLog(@"Invalid json: %@", [error localizedDescription]);
+            [self invalidJson:error];
             continue;
         }
         NSDictionary *dict = object;
         NSString *command = dict[@"command"];
-        if ([command isEqualToString:@"set-speed"]) {
-            Vehicle *v = [self getVehicle:dict[@"carId"]];
-            int val = (int)[dict[@"value"] integerValue];
-            [v setSpeed:val];
-        }
-        else if ([command isEqualToString:@"set-offset"]) {
-            Vehicle *v = [self getVehicle:dict[@"carId"]];
-            float val = (int)[dict[@"value"] floatValue];
-            NSLog(@"OFFSET: **%f**\n", val);
-            [v setLaneOffset:val];
-        }
-        else if ([command isEqualToString:@"list-cars"]) {
-            NSMutableArray *cars = [NSMutableArray new];
-            for (Vehicle *v in _controller.vehicles) {
-                NSMutableDictionary *car = [NSMutableDictionary new];
-                [car setObject:[NSNumber numberWithUnsignedInt:v.identifier] forKey:@"id"];
-                [cars addObject:car];
-            }
-            NSData *jsonData = [NSJSONSerialization dataWithJSONObject:cars options:0 error:&error];
-            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            NSLog(@"**%@**\n", jsonString);
-        }
-        else if ([command isEqualToString:@"spin"]) {
-            Vehicle *v = [self getVehicle:dict[@"carId"]];
-            [v spin180];
-        }
-        else if ([command isEqualToString:@"scan"]) {
-            [_controller beginScan];
-        }
-        else if ([command isEqualToString:@"stop-scan"]) {
-            [_controller stopScan];
-        }
-        else {
-            NSLog(@"Unrecognized command %@", command);
-        }
+        [self perform:command:dict:error];
     }
+}
+
+-(void) perform:(NSString*)command :(NSDictionary*)dict :(NSError*)error {
+    if ([command isEqualToString:@"set-speed"])  { [self setSpeed:dict];  }
+    else if ([command isEqualToString:@"set-offset"]) { [self setOffset:dict]; }
+    else if ([command isEqualToString:@"list-cars"])  { [self listCars:error]; }
+    else if ([command isEqualToString:@"spin"])       { [self spin:dict];      }
+    else if ([command isEqualToString:@"scan"])       { [self beginScan];      }
+    else if ([command isEqualToString:@"stop-scan"])  { [self stopScan];       }
+    else {
+        [self unrecognized:command];
+    }
+}
+
+-(void) invalidJson:(NSError*)error {
+    NSLog(@"Invalid json: %@", [error localizedDescription]);
+}
+
+-(void) setSpeed:(NSDictionary*) dict {
+    Vehicle *v = [self getVehicle:dict[@"carId"]];
+    int val = (int)[dict[@"value"] integerValue];
+    [v setSpeed:val];
+}
+
+-(void) setOffset:(NSDictionary*) dict {
+    Vehicle *v = [self getVehicle:dict[@"carId"]];
+    float val = (int)[dict[@"value"] floatValue];
+    NSLog(@"OFFSET: **%f**\n", val);
+    [v setLaneOffset:val];
+}
+
+-(void) listCars:(NSError*)error {
+    NSMutableArray *cars = [NSMutableArray new];
+    for (Vehicle *v in _controller.vehicles) {
+        NSMutableDictionary *car = [NSMutableDictionary new];
+        [car setObject:[NSNumber numberWithUnsignedInt:v.identifier] forKey:@"id"];
+        [cars addObject:car];
+    }
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:cars options:0 error:&error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+    NSLog(@"**%@**\n", jsonString);
+}
+
+-(void) spin:(NSDictionary*) dict {
+    Vehicle *v = [self getVehicle:dict[@"carId"]];
+    [v spin180];
+}
+
+-(void) beginScan {
+    [_controller beginScan];
+}
+
+-(void) stopScan {
+    [_controller stopScan];
+}
+
+-(void) unrecognized:(NSString*) command {
+    NSLog(@"Unrecognized command %@", command);
 }
 
 -(Vehicle*) getVehicle:(NSNumber*) carId {
